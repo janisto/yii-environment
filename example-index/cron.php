72b89c94a165093e64cb381fc64a5605867e4503
Jani Mikkonen <jani@mikkonen.info>
2011-09-15 15:08:57 +0300

    Comments for console

--- example-index/cron.php
+++ example-index/cron.php
@@ -6,4 +6,8 @@ $env = new Environment();
 $env->init();
 Yii::createConsoleApplication($env->console)->run();
 
+// When you execute a PHP script from the command line, it inherits the environment variables defined in your shell.
+// That means you can set an environment variable using the export command like so:
+// export YII_ENVIRONMENT='TEST'
+
 // Shell: php /path/to/cron.php command
